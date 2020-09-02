cfaf1826fced91198ce93643933a106c0689d1de
Marcelo Hexsel <marcelo.hexsel@hotmail.com>
2020-09-02 08:19:49 -0300

    Commit inicial

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,72 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="form-group">
+      <label>CPF sem máscara</label>
+      <div>
+        <input type="text" v-model="cpf" />
+      </div>
+    </div>
+    <div class="form-group">
+      <label>CPF</label>
+      <div>
+        <the-mask
+          :masked="true"
+          v-model="cpfComMascara"
+          :mask="'###.###.###-##'"
+        />
+      </div>
+    </div>
+    <div class="form-group">
+      <label>Telefone (8 ou 9 dígitos)</label>
+      <div>
+        <the-mask
+          v-model="telefoneMask"
+          :mask="['(##) ####-####', '(##) #####-####']"
+        />
+      </div>
+    </div>
+    <div class="form-group">
+      <label>Informe a sua idade</label>
+      <div>
+        <el-input-number
+          v-model="idade"
+          :min="1"
+          :max="150"
+          placeholder="idade"
+          size="small"
+        ></el-input-number>
+      </div>
+    </div>
+    <div class="form-group">
+      <label>Data de nascimento</label>
+      <div>
+        <el-date-picker
+          v-model="dataNascimento"
+          type="date"
+          placeholder="Data nascimento"
+          format="dd/MM/yyyy"
+          value-format="dd/MM/yyyy"
+          size="small"
+        >
+        </el-date-picker>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      cpf: "",
+      cpfComMascara: "",
+      telefoneMask: "5199995555",
+      idade: 1,
+      dataNascimento: null,
+    };
+  },
+};
 </script>
 
 <style>
@@ -21,8 +74,6 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
+import VueTheMask from "vue-the-mask";
+import ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+import locale from "element-ui/lib/locale/lang/pt-br";
 
-Vue.config.productionTip = false
+Vue.use(ElementUI, { locale });
+Vue.use(VueTheMask);
+
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
